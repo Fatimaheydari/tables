48c3ae14943e291c88e7199ec3bd7430b33f2af9
Fatimaheydari <130399467+Fatimaheydari@users.noreply.github.com>
2023-05-19 21:33:12 +0330

    Add files via upload

---
+++ dbo.azemayeshgah.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Laboratory] (
+    [Id]           INT           NULL,
+    [Type of tool] NVARCHAR (30) NOT NULL,
+    [Number]       INT           NOT NULL,
+    [Appearance]   NVARCHAR (30) NOT NULL,
+    [color]        NVARCHAR (15) NULL,
+    [Application]  NVARCHAR (30) NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
